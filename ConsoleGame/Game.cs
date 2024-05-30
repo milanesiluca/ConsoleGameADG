4bcc30d9d0e2651d0a7dfb857e4f7d54f0f30727
milanesiluca <luca.milanesi@live.se>
2024-05-30 14:23:22 +0200

    Add project files.

---
+++ ConsoleGame/Game.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.Data;
+
+internal class Game
+{
+    public Game()
+    {
+    }
+
+    internal void Run()
+    {
+        Inizialize();
+    }
+
+    private void Inizialize()
+    {
+        var map = new Map(width: 10, height: 10);
+    }
+}
